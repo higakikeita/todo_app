77662531971e8b58bb9f69e068898163d2edd4d9
@higakikeita <higaki.keichan@gmail.com>
2020-02-22 23:38:27 +0900

    jsファイル編集

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -2,12 +2,10 @@ $(function () {
   function buildHTML(todo) {
     var html = $('<li class="todo">').append(todo.content);
     return html;
-    console.log('kotaro')
   }
   $('.js-form').on('submit', function (e) {
     e.preventDefault();
     var todo = $('.js-form__text-field').val();
-    console.log('ayaya')
     $.ajax({
       type: 'POST',
       url: '/todos.json',
@@ -20,7 +18,6 @@ $(function () {
     })
       .done(function (data) {
         var html = buildHTML(data);
-
         $('.todos').append(html);
         $('.js-form__text-field').val('');
       })
